ac89d2226ed62dad9afb9d3dc1437e4c06519026
hoboJF <jadenfiero@gmail.com>
2025-09-07 21:42:22 -0400

    played around with comments, printing and wrote a simple function to find the average of 3 numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    //dude i wrote a comment HOLY SHIT THIS IS CRAZY!!!
+    /* this comment spans multiple lines, wicked
+    why am i writing this */
+    let x = 10;
+    println!("x is {}", x);
+    average();
+}
+
+fn average(){
+    //challenge from the linkedin course
+    let a = 13;
+    let b = 2.3;
+    let c: f32 = 120.0;
+    let average = (a as f64 + b + c as f64)/3.0;
+    println!("the average of a, b and c is {}", average);
 }
