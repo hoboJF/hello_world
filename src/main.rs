b4b1c4c54735975e27bf35e68faa510847b4bae4
hoboJF <jadenfiero@gmail.com>
2025-09-08 03:09:50 -0400

    wrote a simple function to convert celsius to fahrenheit

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ fn main() {
     let x = 10;
     println!("x is {}", x);
     average();
+    //code under this comment is for one of the linkedin challenges
+    let celsius_temp = 23.0;
+    let fahrenheit_temp = celsius_to_fahrenheit(celsius_temp);
+    println!("the temperature in fahrenheit is {}", fahrenheit_temp)
 }
 
 fn average(){
@@ -14,4 +18,8 @@ fn average(){
     let c: f32 = 120.0;
     let average = (a as f64 + b + c as f64)/3.0;
     println!("the average of a, b and c is {}", average);
+}
+
+fn celsius_to_fahrenheit(temperature : f64) -> f64{
+    (1.8 * temperature) + 32 as f64
 }
